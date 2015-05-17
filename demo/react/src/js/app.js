c8bd92d104dd77ef2672018db637f357bd2b1aea
Ilnur Khalilov <xomyaq@gmail.com>
2015-05-18 00:25:27 +0300

    Popup auto open on thanks page

--- demo/react/src/js/app.js
+++ demo/react/src/js/app.js
@@ -4,7 +4,6 @@ import Oldi from './components/Oldi.jsx';
 const appRoot = document.getElementById('sp-loyalty-app');
 
 const PARTNER_ID = appRoot.getAttribute('data-partner-id');
-const DISPLAY = appRoot.getAttribute('data-display') !== null;
 const PAGE = appRoot.getAttribute('data-page') !== 'undefined' ? appRoot.getAttribute('data-page') : '';
 const DOMAIN = appRoot.getAttribute('data-domain') !== 'undefined' ? appRoot.getAttribute('data-domain') : 'https://sailplay.ru';
 const ORDER_NUM = appRoot.getAttribute('data-order-num');
@@ -13,7 +12,7 @@ const PRICE = appRoot.getAttribute('data-price');
 React.render(
   <Oldi
     partnerId={PARTNER_ID}
-    show={DISPLAY}
+    show={PAGE === 'thanks'}
     page={PAGE}
     domain={DOMAIN}
     orderNum={ORDER_NUM}
