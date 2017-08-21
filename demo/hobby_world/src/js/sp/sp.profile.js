6d80c9e05b9834680c00de0de25ce8d9454c3093
Vladimir <hibonicus1@gmail.com>
2017-08-21 18:50:34 +0300

    hw carousel fix

--- demo/hobby_world/src/js/sp/sp.profile.js
+++ demo/hobby_world/src/js/sp/sp.profile.js
@@ -203,7 +203,7 @@ angular.module('sp.profile', [])
 
         scope.actions = sp_api.data('load.actions.list');
 
-        scope.user = sp_api.data('load.user.info');z
+        scope.user = sp_api.data('load.user.info');
 
         scope.form = spProfile.getForm();
 
