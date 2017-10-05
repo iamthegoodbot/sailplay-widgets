7ce1e99548155d283dceda5d415cad2b837ef338
Vladimir <hibonicus1@gmail.com>
2017-10-05 19:29:19 +0300

    jpc script logic update

--- demo/jpc_referral/jpcrefer.js
+++ demo/jpc_referral/jpcrefer.js
@@ -2,15 +2,15 @@ window.sp_share = function(params){
 	
 
   SAILPLAY.send('init', { 
-    partner_id: params.partner_id || 232, 
-    domain: params.domain || 'http://dev2.sailplay.ru' 
+    partner_id: params.partner_id, 
+    domain: params.domain
   })
 
   var referralLink = void 0
   var referralName = void 0
 
   SAILPLAY.on('init.success', function(){
-    SAILPLAY.send('login', params.auth_hash || 'bf06dfe9737b48c11db9aada340f97159ed89703');
+    SAILPLAY.send('login', params.auth_hash);
   });
 
 
@@ -18,19 +18,23 @@ window.sp_share = function(params){
   SAILPLAY.on('login.success', function(data){
     referralName = data.name
     SAILPLAY.send('user.referral', {}, function(data){
-      referralLink = (params.base_referral_link ? params.base_referral_link : params.domain).replace(/\/$/, '') + data.referral_link
+      referralLink = (params.base_referral_link ? params.base_referral_link : params.domain).replace(/\/$/, '') + data.referrer
       console.log(referralLink)
     })
   })
 
   var testform = function(email, tagname, cb){
-    SAILPLAY.send('tags.add', {user: {email: email}, tags:[tagname]}, function(){
-      SAILPLAY.send('vars.add', {user: {email: email}, custom_vars:{
+    SAILPLAY.send('vars.add', {user: {email: email}, custom_vars:{
         referrer_link: referralLink,
         referrer_name: referralName
       }}, function(){
-        cb && cb()
-      })
+        SAILPLAY.send('tags.add', {user: {email: email}, tags:[tagname]}, function(){
+          cb && cb()
+          if(!params.prevent_form_send){
+            document.getElementById(params.form_id)
+              .submit()
+          }
+        })
     })
   }
   var createByEmail = function(email, cb){
@@ -42,9 +46,7 @@ window.sp_share = function(params){
     document.getElementById(params.form_id).addEventListener('submit', function(ev){
       createByEmail(ev.target[0].value, function(){'callback'})
       ev.target[0].value = ''
-      if(params.prevent_form_send){
-        ev.preventDefault()
-      }
+      ev.preventDefault()
     })
 
   })
